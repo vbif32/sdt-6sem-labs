892c8cb4200974c8e948af766287c785b483bd77
dartaan <vbif.32@ya.ru>
2017-05-10 10:51:15 +0300

    Lab 5 complete

---
+++ LabFive/LabFive.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LabFive
+{
+    class LabFive
+    {
+        private const string Separator = "--------------------------------------------------";
+        private const string Welcome = "Выберите нужно распознать строку или поменять 2 слова местами(1/2). Для выхода введите 'q'.";
+        private const string PartOne = "1";
+        private const string PartTwo = "2";
+        private const string Quit = "q";
+        private const string Error = "Ошибочный ввод";
+        private const string Input = "Введите строку для проверки";
+        
+        //static void Main(string[] args)
+        //{
+        //    while (true)
+        //    {
+        //        Console.WriteLine(Separator);
+        //        Console.WriteLine(Welcome);
+        //        var partNumber = Console.ReadLine();
+        //        if (partNumber == PartOne)
+        //        {
+        //            Console.WriteLine(Input);
+        //            Console.WriteLine(Patterns.WhatIsIt(Console.ReadLine().Trim()));
+        //        }
+        //        else if (partNumber == PartTwo)
+        //        {
+        //            Console.WriteLine(Input);
+        //            var s = Console.ReadLine().Trim();
+        //            Console.WriteLine(s.Contains(' ') ? Patterns.Swap(s) : Error);
+        //        }
+        //        else if (partNumber == Quit)
+        //            break;
+        //        else
+        //            Console.WriteLine(Error);
+        //    }
+        //}
+
+        static void Main(string[] args)
+        {
+            var RecognizeTestStrings = new List<string>
+            {
+                "31/12/2017",
+                "12:00:00",
+                "31/12/2017 12:00:00",
+                "test@test.com",
+                "test.com",
+                "just string"
+            };
+            
+            foreach(var s in RecognizeTestStrings)
+                Console.WriteLine(s + " " + Patterns.WhatIsIt(s));
+
+            var SwapTestStrings = new List<string>
+            {
+                "раз два"
+            };
+
+            foreach (var s in SwapTestStrings)
+                Console.WriteLine(Patterns.Swap(s));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ LabFive/Patterns.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace LabFive
+{
+    public static class Patterns
+    {
+        public const string Date_YYYYMMDD = @"[0-9]{4}/(0[1-9]|1[012])/(0[1-9]|1[0-9]|2[0-9]|3[01])";
+        public const string Date_DDMMYYYY = @"(0[1-9]|1[0-9]|2[0-9]|3[01])/(0[1-9]|1[012])/[0-9]{4}";
+        public const string Time_HHMMSS = @"(0[0-9]|1[0-9]|2[0-3])(:[0-5][0-9]){2}";
+        public const string Email = @"^[-\w.]+@([A-z0-9][-A-z0-9]+\.)+[A-z]{2,4}$";
+        public const string Domen = @"([a-zA-Z0-9]([a-zA-Z0-9\-]{0,61}[a-zA-Z0-9])?\.)+[a-zA-Z]{2,6}";
+
+        private static readonly Regex DateRegex1 = new Regex(Date_DDMMYYYY);
+        private static readonly Regex DateRegex2 = new Regex(Date_YYYYMMDD);
+        private static readonly Regex TimeRegex = new Regex(Time_HHMMSS);
+        private static readonly Regex EmailRegex = new Regex(Email);
+        private static readonly Regex DomenRegex = new Regex(Domen);
+
+        private const string ItIs = "Эта строка содержит";
+        private const string ItIsDate = " дату";
+        private const string ItIsTime = " время";
+        private const string ItIsEmail = " email";
+        private const string ItIsDomen = " доменное имя";
+        private const string ItIsUnknown = "Я не знаю, что это";
+
+        public static string WhatIsIt(string s)
+        {
+            var rv = ItIs;
+            if (DateRegex1.IsMatch(s) || DateRegex2.IsMatch(s))
+               rv += ItIsDate;
+            if (TimeRegex.IsMatch(s))
+               rv += ItIsTime;
+            if (EmailRegex.IsMatch(s))
+                rv += ItIsEmail;
+            if (DomenRegex.IsMatch(s))
+                rv += ItIsDomen;
+            return rv == ItIs ? ItIsUnknown : rv;
+        }
+
+        public static string Swap(string s) => s.Split(' ').Reverse().Aggregate((accumulate, i) => accumulate + " " + i);
+    }
+}
