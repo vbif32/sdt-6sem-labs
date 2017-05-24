5c25786661d2dbc440c21d2a35983c94b43a9ffb
dartaan <vbif.32@ya.ru>
2017-05-25 00:27:07 +0300

    add exam

---
+++ Exam/ExamTask.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Exam
+{
+    static class ExamTask
+    {
+        public static int CountMaxSequence(IEnumerable<int> arr)
+        {
+            var min = arr.Min();
+            var max = arr.Max();
+            var size = max - min + 1;
+            if (size < 2)
+                return arr.Count();
+            var counts = new int[size];
+
+            foreach (var item in arr)
+                counts[item - min]++;
+
+            var result = counts[0];
+            for (int i = 0; i < counts.Length - 1; i++)
+            {
+                var tmp = counts[i] + counts[i + 1];
+                if (tmp > result)
+                    result = tmp;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Exam/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Exam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите размер массива");
+            //var count = int.Parse(Console.ReadLine());
+            var count = 100;
+            Console.WriteLine("Введите массив чисел в одну строку, разделяя элементы пробелом");
+            //var digitsStrings = Console.ReadLine().Split();
+            var digitsStrings = (
+                                 "66 66 66 66 66 66 66 66 66 66 66 66 66 66 66 66 66 66 66 66 66 66 66 66 66 " +
+                                 "66 66 66 66 66 66 66 66 66 66 66 66 66 66 66 66 66 66 66 66 66 66 66 66 66 " +
+                                 "66 66 66 66 66 66 66 66 66 66 66 66 66 66 66 66 66 66 66 66 66 66 66 66 66 " +
+                                 "66 66 66 66 66 66 66 66 66 66 66 66 66 66 66 66 66 66 66 66 66 66 66 66 66 "
+                                 ).Split();
+            var digitsInts = new List<int>(count);
+            for (int i = 0; i < count; i++)
+                digitsInts.Add(int.Parse(digitsStrings[i]));
+
+            Console.WriteLine(ExamTask.CountMaxSequence(digitsInts));
+            Console.ReadKey();
+        }
+    }
+}
